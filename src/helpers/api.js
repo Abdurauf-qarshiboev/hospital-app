b1920bb729f50109fe2fcdcc728505cd6646f2f4
Abdurauf Qarshiboyev <abduraufqarshiboyev23092005@gmail.com>
2024-11-14 02:57:21 +0500

    api changed

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const url = 'http://195.158.9.124:4109'
-const url = "https://hospitalapi.talk2me.uz";
+const url = 'http://195.158.9.124:4109'
+// const url = "https://hospitalapi.talk2me.uz";
 const api = axios.create({
   baseURL: url,
 });
